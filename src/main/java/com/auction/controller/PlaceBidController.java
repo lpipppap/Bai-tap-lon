0d75ce8bc3a102cdd7aaca2e22e35ae91386d54a
Quyên <duyanhdao099@gmail.com>
2026-05-30 02:35:30 +0700

    PlaceBidController

--- src/main/java/com/auction/controller/PlaceBidController.java
+++ src/main/java/com/auction/controller/PlaceBidController.java
@@ -1,12 +1,17 @@
 package com.auction.controller;
 
 import com.auction.auction.Auction;
+import com.auction.manager.AuctionManager;
+import com.auction.model.Bidder;
+import com.auction.model.User;
 import com.auction.util.SceneUtil;
+import com.auction.util.SessionManager;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 
 public class PlaceBidController {
     @FXML private Label name;
@@ -18,12 +23,23 @@ public class PlaceBidController {
     @FXML private Button placeBid;
     @FXML private Label warning;
 
+    private Auction auctionnn;
+
+    @FXML
+    private void initialize() {
+        User user = SessionManager.getCurrentUser();
+        if (!(user instanceof Bidder)) {
+            placeBid.setDisable(true);
+        }
+    }
+
     @FXML
     private void backToMenu(ActionEvent event) {
         SceneUtil.changeScene(event, "/com/auction/view/AuctionMenu.fxml");
     }
 
     public void setData(Auction auction) {
+        this.auctionnn = auction;
         name.setText("Product name: " + auction.getItem().getName());
         startPrice.setText("Start price: " + auction.getItem().getStartPrice());
         currentPrice.setText("Highest price: " + auction.getItem().getCurrentPrice());
@@ -33,6 +49,18 @@ public class PlaceBidController {
 
     @FXML
     private void placeBidAction() {
+        String price = enterPrice.getText();
+
+        try {
+            if (AuctionManager.getInstance().checkBid(auctionnn, price)) {
+                warning.setTextFill(Color.web("#02fdba"));
+                warning.setText("Bid is placed successfully!");
+            } else throw new Exception();
+        } catch (IllegalArgumentException e) {
+            warning.setText(e.getMessage());
+        } catch (Exception e) {
+            warning.setText("Error");
+        }
     }
 
 }
