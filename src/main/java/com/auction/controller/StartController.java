73ebc03c494537ad4cc631f43cc9574b36d22b4a
Quyên <duyanhdao099@gmail.com>
2026-08-25 15:10:39 +0700

    logo chibi lick lick lick in start menu

--- src/main/java/com/auction/controller/StartController.java
+++ src/main/java/com/auction/controller/StartController.java
@@ -3,8 +3,17 @@ package com.auction.controller;
 import javafx.fxml.FXML;
 import com.auction.util.SceneUtil;
 import javafx.event.ActionEvent;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class StartController {
+    @FXML ImageView imageView;
+
+    @FXML
+    public void initialize() {
+        imageView.setImage(new Image("https://res.cloudinary.com/kurylrtx/image/upload/v1787569816/IMG_6809_uj8czc.jpg", true));
+    }
+
     @FXML
     private void loginAction(ActionEvent event) {
         SceneUtil.changeScene(event, "/com/auction/view/Login.fxml");
